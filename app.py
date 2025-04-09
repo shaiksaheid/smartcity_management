e7fd914206e198d59062e87bce7a4a740ece85f3
shaiksaheid <shaikshaheid8@gmail.com>
2025-04-09 22:06:44 +0530

    Add gunicorn to requirements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_mysqldb import MySQL
 import os
+from flask import Flask
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -212,7 +213,6 @@ def admin_logout():
     return redirect(url_for('login'))  # redirect to user login page
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port, debug=False)
